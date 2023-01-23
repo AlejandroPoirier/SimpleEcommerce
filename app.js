36532d70af6e008a05c7c0441bb1a9497d3aea6d
Alejandro GPA <ale.poirier16@gmail.com>
2023-01-23 19:40:05 -0300

    Add order and order-item models

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
+const Product = require('./models/product');
+const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
+const Order = require('./models/order');
+const OrderItem = require('./models/order-item');
 
 const app = express();
 
@@ -14,17 +20,16 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req,res,next) => {
-    User.findByPk(1)
-        .then(user=>{
-            req.user = user;
-            next();
-        })
-        .catch(err=>console.log(err));
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
 });
 
 app.use('/admin', adminRoutes);
@@ -32,49 +37,36 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-//======= Associating relations between tables =======
-const Product = require('./models/product');
-const User = require('./models/user');
-const Cart = require('./models/cart');
-const CartItem = require('./models/cart-item');
-const Order = require('./models/order');
-const OrderItem =  require('./models/order-item');
-
-Product.belongsTo(User, {constraints: true, onDelete: "CASCADE"});
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
 User.hasMany(Product);
-
-Cart.belongsTo(User, {constraints: true, onDelete: 'CASCADE'});
 User.hasOne(Cart);
-
-Cart.belongsToMany(Product, { through: CartItem});
-Product.belongsToMany(Cart, { through: CartItem});
-
-Order.belongsTo(User, {constraints: true, onDelete:"CASCADE"});
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
+Order.belongsTo(User);
 User.hasMany(Order);
-
-Order.belongsToMany(Product, {through: OrderItem});
-Product.belongsToMany(Order, {through: OrderItem});
-
+Order.belongsToMany(Product, { through: OrderItem });
 
 sequelize
-    // .sync({ force: true })
-    .sync()
-    .then( result => {
-        return User.findByPk(1)
-    })
-    .then(user => {
-        if (!user) {
-            User.create({id: 1, name:"Dummy User", email:"dummyuser@mail.com"});
-        }
-        return Promise.resolve(user)
-    })
-    .then(user => {
-        return user.createCart()
-    })
-    .then(cart => {
-        app.listen(3000)
-    })
-    .catch( err => {
-        console.log(err)
-    });
-
+  // .sync({ force: true })
+  .sync()
+  .then(result => {
+    return User.findByPk(1);
+    // console.log(result);
+  })
+  .then(user => {
+    if (!user) {
+      return User.create({ name: 'Alejandro', email: 'test@test.com' });
+    }
+    return user;
+  })
+  .then(user => {
+    // console.log(user);
+    return user.createCart();
+  })
+  .then(cart => {
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
--- util/database.js
+++ util/database.js
@@ -1,24 +1,8 @@
-const Sequelize = require("sequelize");
+const Sequelize = require('sequelize');
 
-//Database info
-const dbName = "node-complete"
-const userName = "root"
-const dbPass = "BDcontra2022"
-
-
- const sequelize = new Sequelize(dbName, userName, dbPass, {
-	dialect: 'mysql',
-	host: 'localhost'
+const sequelize = new Sequelize('node-complete', 'root', 'BDcontra2022', {
+  dialect: 'mysql',
+  host: 'localhost'
 });
 
-
-sequelize
-	.authenticate()
-	.then( () => {
-		console.log("Base de datos conectada correctamente")
-	})
-	.catch( err => {
-		console.log('Error al conectar a la base de datos:', err)
-	});
-	
 module.exports = sequelize;
